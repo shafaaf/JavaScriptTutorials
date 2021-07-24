af6ae9bc5c9555b2f4f5a29ec394f5afce71d4c3
shafaaf <shafaaf.hossain@hotmail.com>
2021-07-23 20:06:18 -0400

    sdaf

--- testing/react/react-testing-library/enzyme-vs-react-testing-library/src/App.react-testing-library.test.js
+++ testing/react/react-testing-library/enzyme-vs-react-testing-library/src/App.react-testing-library.test.js
@@ -5,7 +5,7 @@ import {waitFor} from "@testing-library/react";
 import React from "react";
 
 test('entering a todo in form adds a todo', async () => {
-    const { getByText, getByPlaceholderText, getByTestId, container } = render(<App />);
+    const { getByText, getByPlaceholderText, getByTestId} = render(<App />);
 
     // enter todo text in textbox
     getByPlaceholderText('Enter todo text').value = 'My new todo';
@@ -14,11 +14,12 @@ test('entering a todo in form adds a todo', async () => {
     Simulate.click(getByText('Add'));
 
     // wait for todo to show up
+    // https://stackoverflow.com/questions/65725591/react-testing-library-how-to-use-waitfor
     await waitFor(() => getByText('My new todo'));
 
     // make sure form is cleared
     expect(getByTestId('TodoForm-input').value).toEqual('');
 
-    // make sure todo was added
+    // make sure the todo was added
     expect(getByText('My new todo')).toBeDefined();
 });
