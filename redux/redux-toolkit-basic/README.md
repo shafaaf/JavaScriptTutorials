3823c6aa52ed8cf2db1d84eacf6704a720f545d5
shafaaf <shafaaf.hossain@hotmail.com>
2021-09-06 16:48:42 -0400

    ifsdan

---
+++ redux/redux-toolkit-basic/README.md
@@ -0,0 +1,8 @@
+## Redux toolkit basics
+
+From: https://www.youtube.com/watch?v=iBUJVy8phqw&ab_channel=TheNetNinja
+Code: https://codesandbox.io/s/modest-https-h7x3m?file=%2Fsrc%2Fredux%2Fcounter.js%3A125-126
+
+Following redux toolkit doc:
+https://redux-toolkit.js.org/tutorials/quick-start
+
---
+++ redux/redux-toolkit-basic/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {increment, decrement, incrementByAmount} from "./redux/counter";
+
+const App = () => {
+    const {count} = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+    return (
+        <>
+            <h1>The count is {count}</h1>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(incrementByAmount(33))}>Increment by 33</button>
+        </>
+    )
+}
+
+export default App;
